26b173a33c73a800375ed96821e0d0528cd12f21
Alok kumar Yadav <123951091+Alok-2003@users.noreply.github.com>
2024-02-16 11:06:41 +0530

    Add files via upload

--- src/Components/Support/Support.jsx
+++ src/Components/Support/Support.jsx
@@ -28,9 +28,9 @@ const Support = () => {
 
           <div className="mt-6 flex justify-center ">
             <div className='w-3/4 flex justify-center'>
-              <div className="w-11/12 flex  justify-between">
+              <div className="w-11/12 flex  justify-between text-lg">
                 <div className="header text-white">
-                  <h1 className='px-3  bg-[#4C00A4] rounded-full '> Options:</h1>
+                  <h1 className='px-3 py-2  bg-[#4C00A4] rounded-full '> Options:</h1>
                 </div>
                 <div className="w-2/3 flex options justify-between  ">
                   <select id="year" className='border-2 border-gray-500 shadow-2xl px-4 rounded-full'>
@@ -54,23 +54,23 @@ const Support = () => {
           <div className="mt-2 flex justify-center">
             <div className="w-2/3 grid grid-cols-3 ">
               {/* First column: Event details */}
-              <div className="w-11/12 p-4">
-                <label htmlFor="title" className="mt-3 block text-sm font-medium text-gray-700">Subject:</label>
-                <label htmlFor="title" className="mt-3 block text-sm font-medium text-gray-700">Description:</label>
-                <label htmlFor="title" className="mt-3 block text-sm font-medium text-gray-700">Recive OTP on:</label>
-                <label htmlFor="title" className="mt-3 block text-sm font-medium text-gray-700">Documents(if any):</label>
+              <div className="w-11/12 p-4 text-lg">
+                <label htmlFor="title" className="mt-3 block  font-medium text-gray-700">Subject:</label>
+                <label htmlFor="title" className="mt-3 block  font-medium text-gray-700">Description:</label>
+                <label htmlFor="title" className="mt-3 block  font-medium text-gray-700">Recive OTP on:</label>
+                <label htmlFor="title" className="mt-3 block  font-medium text-gray-700">Documents(if any):</label>
               </div>
 
               {/* Second column: Input fields */}
               <div className="w-11/12 p-4">
-                <div className="mb-4">
+                <div className="mb-4 text-lg">
                   <input
                     type="text"
                     id="title"
                     name="title"
                     // value={eventDetails.title}
                     // onChange={handleInputChange}
-                    className="border-2 mt-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-500 rounded-md"
+                    className="border-2 mt-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm  border-gray-500 rounded-md"
                   />
                   <input
                     type="text"
@@ -78,7 +78,7 @@ const Support = () => {
                     name="title"
                     // value={eventDetails.title}
                     // onChange={handleInputChange}
-                    className="border-2 mt-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-500 rounded-md"
+                    className="border-2 mt-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm  border-gray-500 rounded-md"
                   />
                   <input
                     type="text"
@@ -86,7 +86,7 @@ const Support = () => {
                     name="title"
                     // value={eventDetails.title}
                     // onChange={handleInputChange}
-                    className="border-2 mt-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-500 rounded-md"
+                    className="border-2 mt-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm  border-gray-500 rounded-md"
                   />
                   <input
                     type="file"
@@ -100,9 +100,9 @@ const Support = () => {
               {/* Third column: Poster preview */}
               <div className=" p-4">
                 {/* <h2 className="text-lg font-bold mb-2">Poster Preview</h2> */}
-                <div className=" mb-4">
+                <div className=" mb-4 border-2 h-40 border-gray-400 rounded-md flex items-center justify-center">
                   {/* Display poster preview */}
-                  {posterPreview && <img src={posterPreview} alt="Poster Preview" className="w-full h-auto" />}
+                  {posterPreview && <img src={posterPreview} alt="Poster Preview" className="w-full object-center object-contain " />}
                 </div>
                 {/* Input for uploading poster image */}
 
